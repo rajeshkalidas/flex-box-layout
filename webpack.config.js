a434c056519f783aac69d2b51568f14957b55c72
rajeshkalidas <rajeshkumar.kalidas@gmail.com>
2019-12-04 09:38:18 +0100

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill', './index.js'],
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     filename: './bundle.js',
     path: path.join(__dirname, 'dist'),
